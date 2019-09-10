fe18a4d3ecaf27f9b05d8a99d49f1dc69f863595
Zhi Li <zli@babson.edu>
2019-09-10 14:15:06 -0400

    Create calc.py

---
+++ session01/calc.py
@@ -0,0 +1,17 @@
+minutes = 42
+seconds = 42
+
+print('The time duration is', minutes * 60 + seconds, 'seconds.')
+
+# minutes = 42 + 42/60
+# hours = minutes / 60
+
+# km_per_mile = 1.61
+# km = 10
+# miles = km / km_per_mile
+
+# pace = minutes / miles
+# mph = miles / hours
+
+# print('Pace in minutes per mile:', pace)
+# print('Average speed in mph:', mph)
