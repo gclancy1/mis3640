3ee0a5c8119606d0f229a8eef7a15817bbe22bb5
Zhi Li <zli@babson.edu>
2019-09-19 16:34:16 -0400

    finish session 05

--- session04/function-demo.py
+++ session04/function-demo.py
@@ -2,16 +2,20 @@ def print_lyrics():
     print("Hey Jude. Don't make it bad.")
     print("Take a sad song and make it better.")
 
+
 # print_lyrics()
 
 # print(type(print_lyrics))
 
+
 def repeat_lyrics():
     print_lyrics()
-    print('Na - na - na - na - na, na - na - na - na')
+    print("Na - na - na - na - na, na - na - na - na")
     print_lyrics()
 
+
 # repeat_lyrics()
+
 
 def print_twice(name):
     print(name)
@@ -24,6 +28,7 @@ def print_twice(name):
 # his_name = 'Brian'
 # print_twice(his_name)
 # print_twice(42)
+
 
 def cat_twice(part1, part2):
     cat = part1 + part2
@@ -38,8 +43,9 @@ def cat_twice(part1, part2):
 
 
 def give_me_a_break():
-    msg = 'break'
+    msg = "break"
     return msg
+
 
 # print(give_me_a_break())
 # print_twice(give_me_a_break())
@@ -48,16 +54,18 @@ def give_me_a_break():
 
 # print(whatever_function_throws_to_me)
 
+
 def give_me_a_break():
-    str1 = 'break'    
+    str1 = "break"
     return str1
-    
+
+
 # print(give_me_a_break())
 
 
 from my_abs import brian_abs
 
-print(brian_abs('A'))
+print(brian_abs("A"))
 
 # def f1():
 #     pass
---
+++ session05/mypolygon.py
@@ -0,0 +1,82 @@
+import turtle
+import math
+
+brian = turtle.Turtle()
+
+brian.pensize(10)
+print(brian)
+
+# for _ in range(4):
+#     brian.fd(100)
+#     brian.lt(90)
+
+
+def square(t, length):
+    for i in range(4):
+        t.fd(length)
+        t.lt(90)
+
+
+# def polygon(t, length, n):
+#     for i in range(n):
+#         t.fd(length)
+#         t.lt(360 / n)
+
+
+def circle(t, r):
+    circumference = 2 * math.pi * r
+    n = 50
+    length = circumference / n
+    polygon(t, length, n)
+
+
+# def arc(t, r, angle):
+#     """
+#     Draw an arc with radius, r, and angle (in degree)
+#     """
+#     arc_length = 2 * math.pi * r * angle / 360
+#     n = int(arc_length / 3) + 1
+#     step_length = arc_length / n
+#     step_angle = angle / n
+
+#     for i in range(n):
+#         t.fd(step_length)
+#         t.lt(step_angle)
+
+
+def polyline(t, n, length, angle):
+    for i in range(n):
+        t.fd(length)
+        t.lt(angle)
+
+
+def polygon(t, n, length):
+    angle = 360 / n
+    polyline(t, n, length, angle)
+
+
+def arc(t, r, angle):
+    arc_length = 2 * math.pi * r * angle / 360
+    n = int(arc_length / 3) + 1
+    step_length = arc_length / n
+    step_angle = float(angle) / n
+    polyline(t, n, step_length, step_angle)
+
+
+def move(t, x, y):
+    t.pu()
+    t.setpos(x, y)
+    t.pd()
+
+# square(brian, 200)
+# polygon(brian, 8, 100)
+# polygon(brian, n=8, length=100)
+# circle(brian, 100)
+
+# arc(brian, 100, 90)
+arc(brian, 100, 360)
+move(brian, -200, 100)
+polygon(brian, 3, 100)
+
+
+turtle.mainloop()
