3f76119385387f36f15127198e79c32712592850
Zhi Li <zli@babson.edu>
2019-09-26 14:45:51 -0400

    add solutions to exercises 06

---
+++ session06/exercises/greatest_common_divisor.py
@@ -0,0 +1,19 @@
+def gcdRecur(a, b):
+    '''
+    a, b: positive integers
+
+    returns: a positive integer, the greatest common divisor of a & b.
+    '''
+    # Your code here
+    print('Current a, b:', a, b) #for testing
+    if b == 0:
+        return a
+    else:
+        return gcdRecur(b, a % b)
+
+
+if __name__ == "__main__":
+    #print(gcdRecur(2, 12))
+    #print(gcdRecur(6, 12))
+    print(gcdRecur(9, 12))
+    #print(gcdRecur(17, 12))
