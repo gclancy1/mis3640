d8ef63adf00ed184061c8539757325ef622b7e6f
Zhi Li <zli@babson.edu>
2019-10-21 15:33:34 -0400

    Create sed.py

---
+++ session14/exercises/sed.py
@@ -0,0 +1,10 @@
+def sed(pattern, replace, source, dest):
+    """Reads a source file and writes the destination file.
+    In each line, replaces pattern with replace.
+
+    pattern: string
+    replace: string
+    source: string filename
+    dest: string filename
+    """
+    pass
